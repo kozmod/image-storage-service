c660bb8ad525d0a8ecc0e04f59c643eab17ae11a
18388871 <kozmo23g@gmail.com>
2020-09-12 20:02:55 +0300

    add image gallery wrapper

--- src/main/resources/static/js/gallery.js
+++
@@ -1,7 +0,0 @@
-function displayFunk(imgs) {
-    const expandImg = document.getElementById("expandedImg");
-    const imgText = document.getElementById("imgtext");
-    expandImg.src = imgs.src;
-    imgText.innerHTML = imgs.alt;
-    expandImg.parentElement.style.display = "block";
-}
