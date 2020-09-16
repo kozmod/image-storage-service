3d7de42046d97db29e737c27758fc689d55ea50d
kozmod <kozmo23g@gmail.com>
2020-09-16 22:46:59 +0300

    add buttons go get paging img

--- src/main/java/com/github/kozmo/photostorage/controller/DirController.java
+++ src/main/java/com/github/kozmo/photostorage/controller/DirController.java
@@ -1,5 +1,6 @@
 package com.github.kozmo.photostorage.controller;
 
+import com.github.kozmo.photostorage.configuration.AppProperties;
 import com.github.kozmo.photostorage.service.resource.FileResourceLoader;
 import com.github.kozmo.photostorage.service.path.PathLoader;
 import com.github.kozmo.photostorage.service.path.TreeDirPathLoader;
@@ -21,21 +22,28 @@ public class DirController {
     private final static String GALLERY = "fragment/image_gallery";
 
     private final Function<Long, ? extends PathLoader<Collection<Path>>> loaderFactory;
+    private final AppProperties properties;
     private final TreeDirPathLoader treeDirRootLoader;
     private final FileResourceLoader resourceLoader;
 
     public DirController(
             Function<Long, ? extends PathLoader<Collection<Path>>> loaderFactory,
+            AppProperties properties,
             TreeDirPathLoader treeDirRootLoader,
             FileResourceLoader resourceLoader) {
         this.loaderFactory = loaderFactory;
+        this.properties = properties;
         this.treeDirRootLoader = treeDirRootLoader;
         this.resourceLoader = resourceLoader;
     }
 
     @GetMapping("/")
     public String loadPathsFromRootDir(Map<String, Object> model) throws IOException {
-        model.put("dirs", loaderFactory.apply(0L).fromRoot());
+        var dirs = loaderFactory.apply(0L).fromRoot();
+        model.put("leftButton", true);
+        model.put("rightButton", dirs.size() >= properties.getLimit());
+        model.put("limit", properties.getLimit());
+        model.put("dirs", dirs);
         model.put("treeRoot", treeDirRootLoader.fromRoot());
         return DASH;
     }
@@ -44,7 +52,25 @@ public class DirController {
     @GetMapping(value = "/dir/{path}")
     public String getDirImageBlock(@PathVariable("path") String path, Map<String, Object> model) throws IOException {
         path = path.equals("root") ? "" : path;
-        model.put("dirs", loaderFactory.apply(0L).fromSub(Paths.get(path)));
+        var dirs =loaderFactory.apply(0L).fromSub(Paths.get(path));
+        model.put("leftButton", false);
+        model.put("rightButton", dirs.size() >= properties.getLimit());
+        model.put("limit", properties.getLimit());
+        model.put("dirs", dirs);
+        return GALLERY;
+    }
+
+    @GetMapping(value = "/dir/paging/{path}") //todo add ajax function
+    public String getDirImageBlock(@PathVariable("path") String path, @RequestParam Long limit, Map<String, Object> model) throws IOException {
+        var skip = properties.getLimit() + limit;
+        skip = skip < 0 ? 0 : skip;
+
+        path = path.equals("root") ? "" : path;
+        var dirs = loaderFactory.apply(skip).fromSub(Paths.get(path));
+
+        model.put("leftButton", true); //todo think
+        model.put("rightButton", dirs.size() >= properties.getLimit());
+        model.put("dirs", dirs);
         return GALLERY;
     }
 
